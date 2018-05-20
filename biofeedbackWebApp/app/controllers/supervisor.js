ccecc93b5be722019cf7f5726797773e6c52c614
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-05-20 14:32:34 -0500

    msg almost finished

--- biofeedbackWebApp/app/controllers/supervisor.js
+++ biofeedbackWebApp/app/controllers/supervisor.js
@@ -44,15 +44,17 @@ router.post('/cambiar_estado', function (req, res) {
         }
         console.log("ESTO ES: " + req.body.estado_afan + " typeof" + typeof(req.body.estado_afan));
         if (req.body.estado_afan == true) {
+          if (condu.estado_afan == false){
+            //client.messages.create({
+            //  body: 'El estado del conductor '+ condu.nombre +' ha cambiado a afan le recomendamos ponerse en contacto con el: '+condu.telefono,
+            //  from: process.env.TWILIO_PHONE
+            //  to: ' number',
+            //}).then(message => console.log(message.sid)).done();
+          }
             condu.estado_afan = true;
         }else if (req.body.estado_afan == false){
             condu.estado_afan = false;
         }
-        //client.messages.create({
-        //      body: 'This is a biofeedback test message',
-        //      from: 'twilio number',
-        //      to: process.env.TWILIO_PHONE
-        //    }).then(message => console.log(message.sid)).done();
         condu.save(function (err, updatedCond) {
             if (err) return res.send(err);
             return res.json(updatedCond);
